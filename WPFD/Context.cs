e1ac62c1cfab6dcad9d75319d4dcc72c11d74228
megatroneshkereburgerusasagadkaalyaa <123lolbratvagyryvareniyapelmeney@burger.ru>
2024-06-18 21:04:03 +0300

    l

---
+++ WPFD/Context.cs
@@ -0,0 +1,22 @@
+﻿using Pomelo.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPFD
+{
+    public class Context:DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user=root;password=12345;database=posts", new MySqlServerVersion(new Version(8, 4, 0)));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post { PostId=1, Title="Ьарк", Text="l"}
+                );
+        }
+    }
+}
---
+++ WPFD/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Context context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new Context();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.Database.EnsureCreated();
+            context.Posts.Load();
+            datagrid.ItemsSource = context.Posts.Local.ToObservableCollection();
+        }
+    }
+}
